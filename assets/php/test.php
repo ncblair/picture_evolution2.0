8cc6dc7e918478bcd456470fc8734a719c4d02ba
Nathan Blair <nblair@berkeley.edu>
2017-10-12 23:44:33 -0700

    added php testing

---
+++ assets/php/test.php
@@ -0,0 +1,25 @@
+<?php
+    if (!empty($_POST['img'])) {
+        $img = $_POST['img'];
+        echo "hello";
+        return $img;
+    }
+?>
+
+
+<?php
+ +    require_once 'mysql.php';
+ +    $mysql = new Mysql();
+ +    
+ +    if($_POST && !empty($_POST['score']) && !empty($_POST['name'])) {
+ +        $score = $_POST['score'];
+ +        $name = $_POST['name'];
+ +        $result = $mysql->updateHighScore($score, $name);
+ +        echo $result;
+ +    }
+ +    else if ($_POST && !empty($_POST['action'])){
+ +        echo $mysql->currentHigh();
+ +    }
+ +    else if ($_POST && !empty($_POST['action2'])){
+ +        echo $mysql->currentName();
+ +    } 
